498ca71f5ea3ffa5f37287e497529ab1f1569b6c
abdulahad-gh <abdulahadmailacc@gmail.com>
2022-09-28 23:32:28 +0600

    created utils folder, routes folder

---
+++ routes/tools.route.js
@@ -0,0 +1,51 @@
+const express = require('express')
+
+const route = express.Router()
+
+// route.get("/", (req, res) => {
+//     res.send('tools found')
+// })
+// route.post("/", (req, res) => {
+//     res.send('now post api is hit')
+// })
+
+route.route('/')
+
+    /** 
+    * @api {get} get api to get tools
+    * @apiDescription Get all tools from dataBase
+    * @apiPermission Admin
+    *
+    * @apiHeader {String} Authorization User's access token
+    
+    * @apiParam {Number{1-}} [page=1] List name
+    * @apiParam {Number{1-100}} [limit=10] Users per page
+    *
+    * @apiSuccess {Object[]} all the tools
+    *
+    * @apiError (Unauthorized 401) Unauthorized only Authenticated users can access the data
+    * @apiError (Forbidden 403) Forbidden only admins can access the data
+    */
+    .get((req, res) => {
+        res.send('tools found')
+    })
+    /**
+     * @api post api for save tools
+     * @apiDescription This api for save a single tools inside database
+     * @apiPermission only admins
+     * 
+     * @apiHeader {String} Authorization access token (jwt)
+     * 
+     * @apiParam {Number{1-10}} [page=1] list name
+     * @apiParam {Number{1-10}} [page=1] list name
+     * 
+     * @apiSuccess return true when product/tools is include our database
+     * 
+     * @apiError not success
+     * @apiError your product is already exists
+     */
+    .post((req, res) => {
+        res.send('tools found with post method')
+    })
+
+module.exports = route
